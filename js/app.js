2497dffe778e37ae55eccda26d9fbd24a587ffba
muhammed sahal <muhammedsahalap717@gmail.com>
2026-07-22 01:13:43 +0530

    feat: implement dynamic category section with new data source, assets, and styling updates

--- js/app.js
+++ js/app.js
@@ -6,9 +6,38 @@ async function loadComponent(id, file) {
 Promise.all([ // Promise.all waits for all promises to resolve
     loadComponent("navbar", "components/navbar.html"),
     loadComponent("hero", "components/hero.html"),
+    loadComponent("categories", "components/categories.html"),
     loadComponent("footer", "components/footer.html")
 ]).then(() => { // then() executes when all promises are resolved
     if (typeof setupHeroCarousel === "function") {
         setupHeroCarousel();
     }
+    loadCategories();
 });
+
+function loadCategories() {
+    const container = document.getElementById("categoryContainer");
+
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    categories.forEach(category => {
+        container.innerHTML += `
+            <div class="col-6 col-md-4 col-lg">
+                <a href="products.html?category=${category.name.toLowerCase()}" class="text-decoration-none">
+                    <div class="category-card text-center p-4 h-100">
+                        <img
+                            src="${category.image}"
+                            alt="${category.name}"
+                            class="category-img mx-auto mb-3"
+                        >
+                        <h5 class="mb-0 text-dark fw-semibold">
+                            ${category.name}
+                        </h5>
+                    </div>
+                </a>
+            </div>
+        `;
+    });
+}
---
+++ js/data.js
@@ -0,0 +1,22 @@
+const categories = [
+    {
+        name: "Burger",
+        image: "assets/categories/Burger.png"
+    },
+    {
+        name: "Pizza",
+        image: "assets/categories/Pizza.png"
+    },
+    {
+        name: "Tacos",
+        image: "assets/categories/Tacos.png"
+    },
+    {
+        name: "Sushi",
+        image: "assets/categories/Sushi.png"
+    },
+    {
+        name: "Pasta",
+        image: "assets/categories/Pasta.png"
+    }
+];
