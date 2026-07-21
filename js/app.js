e19a7600277e4f349dc07a6ff2e783038bceabf5
muhammed sahal <muhammedsahalap717@gmail.com>
2026-07-22 01:18:24 +0530

    refactor: move category rendering logic to a dedicated categories module

--- js/app.js
+++ js/app.js
@@ -12,32 +12,7 @@ Promise.all([ // Promise.all waits for all promises to resolve
     if (typeof setupHeroCarousel === "function") {
         setupHeroCarousel();
     }
-    loadCategories();
+    if (typeof loadCategories === "function") {
+        loadCategories();
+    }
 });
-
-function loadCategories() {
-    const container = document.getElementById("categoryContainer");
-
-    if (!container) return;
-
-    container.innerHTML = "";
-
-    categories.forEach(category => {
-        container.innerHTML += `
-            <div class="col-6 col-md-4 col-lg">
-                <a href="products.html?category=${category.name.toLowerCase()}" class="text-decoration-none">
-                    <div class="category-card text-center p-4 h-100">
-                        <img
-                            src="${category.image}"
-                            alt="${category.name}"
-                            class="category-img mx-auto mb-3"
-                        >
-                        <h5 class="mb-0 text-dark fw-semibold">
-                            ${category.name}
-                        </h5>
-                    </div>
-                </a>
-            </div>
-        `;
-    });
-}
